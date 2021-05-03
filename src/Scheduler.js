6078e9c8d92d705b5a94679734e31cde37b88da1
Matt Davison <matt45davison@gmail.com>
2021-05-03 00:18:15 -0700

    Add Scheduler react component

---
+++ src/Scheduler.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Scheduler.css';
+
+class Scheduler extends React.Component {
+    render() {
+        return (
+            <div className="Root-scheduler">
+                <div className="H2">
+                    <h2>ELEC</h2>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Scheduler;
